80a53e1a4863d3c5ca43b4a0c5e060edd2c2f7da
AlvinTWNg <alvintwng@gmail.com>
2021-02-17 14:16:14 +0800

    login w bCryth assword

--- src/main/java/carDate/emp/UserDetailImpl.java
+++ src/main/java/carDate/emp/UserDetailImpl.java
@@ -1,63 +1,68 @@
 package carDate.emp;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-//public class UserDetailImpl implements UserDetails {
-//	
-//	private Employee employee;
-//
-//	public UserDetailImpl(Employee employee) {
-//		super();
-//		this.employee = employee;
-//	}
-//	
+public class UserDetailImpl implements UserDetails {
+	
+	private Employee employee;
+
+	public UserDetailImpl(Employee employee) {
+		super();
+		this.employee = employee;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority("USER"));
+	}
+	
 //	@Override
 //	public Collection<? extends GrantedAuthority> getAuthorities() {
-////		Set<Role> roles = employee.getRoles();
-////		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-////		
-////		for (Role role : roles) {
-////			authorities.add(new SimpleGrantedAuthority(role.getName()));
-////
-////		}
-////		
-////		return authorities;
-//		return null;
-//	}
-//	
-//	@Override
-//	public String getPassword() {
-//			return employee.getPassword();
-//	}
-//	
+//		Set<Role> roles = employee.getRoles();
+//		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+//		
+//		for (Role role : roles) {
+//			authorities.add(new SimpleGrantedAuthority(role.getName()));
 //
-//	@Override
-//	public String getUsername() {
-//		return employee.getEmpName();
+//		}
+//		
+//		return authorities;
 //	}
-//	
-//	@Override
-//	public boolean isAccountNonExpired() {
-//		return (employee.getUserExpiry().isAfter(java.time.LocalDate.now()));
-//	}
-//
-//	@Override
-//	public boolean isAccountNonLocked() {
-//		return employee.getIsActive() && (employee.getUserExpiry().isAfter(java.time.LocalDate.now()));
-//	}
-//
-//	@Override
-//	public boolean isCredentialsNonExpired() {
-//		return (employee.getPswdExpiry().isAfter(java.time.LocalDate.now()));
-//	}
-//
-//	@Override
-//	public boolean isEnabled() {
-//		return employee.getIsActive();
-//	}
-//
-//}
+	
+	@Override
+	public String getPassword() {
+			return employee.getPassword();
+	}
+	
+
+	@Override
+	public String getUsername() {
+		return employee.getEmpName();
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return (employee.getUserExpiry().isAfter(java.time.LocalDate.now()));
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return employee.getIsActive() && (employee.getUserExpiry().isAfter(java.time.LocalDate.now()));
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return (employee.getPswdExpiry().isAfter(java.time.LocalDate.now()));
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return employee.getIsActive();
+	}
+
+}
